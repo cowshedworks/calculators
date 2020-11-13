bd25d20cf4b2c3e6d5c2ed35e396a0c5b0ed01c8
Gavin Phillips <gavin@cowshedworks.co.uk>
2020-11-13 18:59:52 +0000

    Add guard

--- src/CalculatorInput.php
+++ src/CalculatorInput.php
@@ -2,18 +2,23 @@
 
 namespace CowshedWorks\Calculators;
 
+use InvalidArgumentException;
+
 class CalculatorInput
 {
     protected array $input;
 
-    public function __construct()
+    public function __construct(array $parameters)
     {
-        $args = func_get_args();
-        $this->input = $args[0];
+        $this->input = $parameters;
     }
 
     public function get($inputKey)
     {
+        if (array_key_exists($inputKey, $this->input) === false) {
+            throw new InvalidArgumentException("{$inputKey} not found in input parameters");
+        }
+            
         return $this->input[$inputKey];
     }
 }
